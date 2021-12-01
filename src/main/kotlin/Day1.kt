4494790c4b78d86181e984454b7b9f1da6e2633b
Adel <576853-astran@users.noreply.gitlab.com>
2021-12-01 23:25:22 +0100

    rework lol

--- src/main/kotlin/Day1.kt
+++ src/main/kotlin/Day1.kt
@@ -1,28 +1,10 @@
 class Day1 {
-    private val InputData: List<Int> = "day1/input".fromResource().readLines().map(String::toInt)
+    private val inputData: List<Int> = "day1/input".fromResource().readLines().map(String::toInt)
 
-    fun count(): Int {
-        return count(InputData)
-    }
+    fun count()= count(inputData)
 
-    fun count3(): Int {
-        return count(InputData.windowed(3, 1) { it.sum() })
-    }
+    fun count3()=count(inputData.windowed(3, 1) { it.sum() })
 
-    private fun count(values: List<Int>): Int {
-        return values.windowed(2, 1) { if (it[1] > it[0]) 1 else 0 }.sum()
-    }
-/*
-    private fun ocount(values: List<Int>): Int {
-        var lastDepth = values.first()
-        var increased = 0
-        for (depth in values) {
-            if (depth > lastDepth) {
-                increased++
-            }
-            lastDepth = depth
-        }
-        return increased
-    }
-*/
+    private fun count(values: List<Int>)= values.windowed(2, 1) { if (it[1] > it[0]) 1 else 0 }.sum()
+
 }
