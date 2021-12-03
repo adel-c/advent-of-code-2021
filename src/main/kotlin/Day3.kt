b0d59adf6963c8f5ba7edf38931fe002d669701d
Adel CHEBBINE <adel.chebbine@external.grtgaz.com>
2021-12-03 11:19:34 +0100

    day 3 1

---
+++ src/main/kotlin/Day3.kt
@@ -0,0 +1,45 @@
+import kotlin.math.pow
+infix fun Int.power(exponent: Int): Int = toDouble().pow(exponent).toInt()
+class Stats(val totalLines: Int, val oneCounts: List<Int> = listOf()) {
+    fun countOnes(line: String): Stats {
+        val map = line.toCharArray().map { it.digitToInt() }
+        val newOneCounts: List<Int> =
+            map.mapIndexed { index, i -> if (oneCounts.size > index) i + oneCounts[index] else i }
+        return Stats(totalLines, newOneCounts);
+    }
+
+    fun mostPresent(): List<Int> {
+        return oneCounts.map { if (it > totalLines / 2) 1 else 0 }
+    }
+
+    fun leastPresent(): List<Int> {
+        return mostPresent().map { if (it == 1) 0 else 1 }
+    }
+}
+
+class Diagnostic(val stats: Stats) {
+    fun gamma(): Int {
+        println(stats.mostPresent())
+        return binaryListToDecimal(stats.mostPresent())
+
+    }
+
+    fun epsilon(): Int {
+        println(stats.leastPresent())
+        return binaryListToDecimal(stats.leastPresent())
+    }
+
+   fun binaryListToDecimal(binAray: List<Int>) = binAray.reversed().reduceIndexed { index, acc, i -> ((2 power index)*i)+acc }
+    //fun binaryListToDecimal(bins: List<Int>) = bins.joinToString("").toInt(2)
+
+}
+
+class Day3 {
+    private val inputData: List<String> = "day3/input".fromResource().readLines()
+
+
+    fun diag(): Diagnostic {
+        val stats = inputData.fold(Stats(inputData.size)) { acc, s -> acc.countOnes(s) }
+        return Diagnostic(stats);
+    }
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,11 +5,26 @@ fun main(args: Array<String>) {
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
 
+    day1()
+    day2()
+    day3()
+}
+
+fun day3() {
+    val day3 = Day3()
+    val diag = day3.diag()
+    println("day3 gamma: ${diag.gamma()}")
+    println("day3 epsilon: ${diag.epsilon()}")
+    println("day3 factor: ${diag.gamma() * diag.epsilon()}")
+}
+
+private fun day1() {
     val day1 = Day1()
     println("day1 count: ${day1.count1()}")
     println("day1 count3: ${day1.count3()}")
+}
 
-
+private fun day2() {
     val day2 = Day2()
     val position = day2.position()
     println("day2 count: $position /Position(horizontal=1895, depth=894)")
