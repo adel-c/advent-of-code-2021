8967c6b7b9563903e49b85db8d3db37767c4936a
Adel CHEBBINE <adel.chebbine@external.grtgaz.com>
2021-12-03 11:56:54 +0100

    day 3 refacto 3 add classes

--- src/main/kotlin/Day3.kt
+++ src/main/kotlin/Day3.kt
@@ -1,33 +1,52 @@
 import kotlin.math.pow
 
 infix fun Int.power(exponent: Int): Int = toDouble().pow(exponent).toInt()
+data class BinaryValue(val data: List<Int>) {
+    companion object {
+        fun fromString(binaryString: String) = BinaryValue(binaryString.toCharArray().map { it.digitToInt() })
+    }
+
+    operator fun get(index: Int): Int {
+        return data[index]
+    }
+
+    override fun toString(): String {
+        return data.joinToString("")
+    }
+
+    fun toDecimal() =
+        data.reversed().reduceIndexed { index, acc, i -> ((2 power index) * i) + acc }
+
+    fun invert() = BinaryValue(data.map { if (it == 1) 0 else 1 })
+}
+
 class Stats(val totalLines: Int, val oneCounts: List<Int> = listOf()) {
-    fun countOnes(line: List<Int>): Stats {
+    fun countOnes(line: BinaryValue): Stats {
 
         val newOneCounts: List<Int> =
-            line.mapIndexed { index, i -> if (oneCounts.size > index) i + oneCounts[index] else i }
+            line.data.mapIndexed { index, i -> if (oneCounts.size > index) i + oneCounts[index] else i }
         return Stats(totalLines, newOneCounts);
     }
 
-    fun mostPresent(): List<Int> {
-        return oneCounts.map { if (it > totalLines / 2) 1 else 0 }
+    fun mostPresent(): BinaryValue {
+        return BinaryValue(oneCounts.map { if (it >= totalLines / 2) 1 else 0 })
     }
 
-    fun leastPresent(): List<Int> {
-        return mostPresent().map { if (it == 1) 0 else 1 }
+    fun leastPresent(): BinaryValue {
+        return mostPresent().invert()
     }
 }
 
-class Diagnostic(val fullData: List<List<Int>>) {
+class Diagnostic(val fullData: List<BinaryValue>) {
     fun gamma(): Int {
         val stats = statFor(fullData);
-        return binaryListToDecimal(stats.mostPresent())
+        return stats.mostPresent().toDecimal()
 
     }
 
     fun epsilon(): Int {
         val stats = statFor(fullData);
-        return binaryListToDecimal(stats.leastPresent())
+        return stats.leastPresent().toDecimal()
     }
 
     fun oxygen(): Int {
@@ -41,17 +60,16 @@ class Diagnostic(val fullData: List<List<Int>>) {
                 it[index] == targetValue
             }
             if (data.size == 1) {
-                return binaryListToDecimal(data.first())
+                return data.first().toDecimal()
             }
         }
         return 0
     }
 
-    fun binaryListToDecimal(binAray: List<Int>) =
-        binAray.reversed().reduceIndexed { index, acc, i -> ((2 power index) * i) + acc }
+
     //fun binaryListToDecimal(bins: List<Int>) = bins.joinToString("").toInt(2)
 
-    fun statFor(data: List<List<Int>>) = data.fold(Stats(data.size)) { acc, s -> acc.countOnes(s) }
+    fun statFor(data: List<BinaryValue>) = data.fold(Stats(data.size)) { acc, s -> acc.countOnes(s) }
 }
 
 class Day3 {
@@ -59,6 +77,6 @@ class Day3 {
 
 
     fun diag(): Diagnostic {
-        return Diagnostic(inputData.map {  line ->line.toCharArray().map { it.digitToInt() } });
+        return Diagnostic(inputData.map { line -> BinaryValue.fromString(line) });
     }
 }
