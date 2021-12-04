c9abd9202ebd0ab15296b10dd558011383ce7d36
Adel <576853-astran@users.noreply.gitlab.com>
2021-12-04 15:34:21 +0100

    add parser Test

--- src/test/kotlin/Day4Test.kt
+++ src/test/kotlin/Day4Test.kt
@@ -5,7 +5,7 @@ class Day4Test {
 
 
     @Test
-    fun diag_test() {
+    fun testParser() {
         val actual = Day4("day4/inputTest").parse()
         assertValues(
             listOf(7, 4, 9, 5, 11, 17, 23, 2, 0, 14, 21, 24, 10, 16, 13, 6, 15, 25, 12, 22, 18, 20, 8, 19, 3, 26, 1),
