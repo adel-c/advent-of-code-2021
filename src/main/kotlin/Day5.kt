4006c603c2986f87dda3c474680d5fe2de56eb32
Adel <576853-astran@users.noreply.gitlab.com>
2021-12-05 12:15:54 +0100

    clean

--- src/main/kotlin/Day5.kt
+++ src/main/kotlin/Day5.kt
@@ -1,34 +1,28 @@
 class Day5(path: String = "day5/input") {
     private val inputData: List<String> = path.fromResource().readLines()
     fun parse(): Puzzle {
-        val lines = inputData.map(Line::from)
+        val lines = inputData.map(this::parseLine)
         return Puzzle(lines)
     }
-
+    private fun parsePoint(string: String): Point {
+        val coord = string.split(",")
+        return Point(coord[0].trim().toInt(), coord[1].trim().toInt())
+    }
+    private fun parseLine(string: String): Line {
+        val points = string.split("->")
+        return Line(parsePoint(points[0]), parsePoint(points[1]))
+    }
 }
 
 data class Point(val x: Int, val y: Int) {
-    companion object {
-        fun from(string: String): Point {
-            val coord = string.split(",")
-            return Point(coord[0].trim().toInt(), coord[1].trim().toInt())
-        }
-    }
 
     fun isDiagonal(p: Point): Boolean = (x != p.x) && (y != p.y)
     fun rangeX(p: Point) = range(x, p.x)
     fun rangeY(p: Point) = range(y, p.y)
-
     private fun range(a: Int, b: Int) = if (a < b) a..b else a downTo b
 }
 
 data class Line(val start: Point, val end: Point) {
-    companion object {
-        fun from(string: String): Line {
-            val points = string.split("->")
-            return Line(Point.from(points[0]), Point.from(points[1]))
-        }
-    }
 
     fun allPoints(ignoreDiagonals: Boolean = true): Set<Point> {
         if (ignoreDiagonals && start.isDiagonal(end)) return setOf()
