7391c98ea432515a3085d812a7f8c17cb762778b
Adel <576853-astran@users.noreply.gitlab.com>
2021-12-05 10:38:00 +0100

    add day5 parser

--- src/main/kotlin/Day5.kt
+++ src/main/kotlin/Day5.kt
@@ -1,10 +1,27 @@
 class Day5(path: String = "day5/input") {
     private val inputData: List<String> = path.fromResource().readLines()
-    fun parse(): Vents {
-        val draws = inputData[0].split(",").map(String::toInt)
-        return Vents(0)
+    fun parse(): Puzzle {
+        val lines = inputData.map(Line::from)
+        return Puzzle(lines)
     }
 
 }
 
-data class Vents(val i: Int)
+
+data class Point(val x: Int, val y: Int) {
+    companion object {
+        fun from(string: String): Point {
+            val coord = string.split(",")
+            return Point(coord[0].trim().toInt(), coord[1].trim().toInt())
+        }
+    }
+}
+data class Line(val start: Point, val end: Point) {
+    companion object {
+        fun from(string: String): Line {
+            val points = string.split("->")
+            return Line(Point.from(points[0]), Point.from(points[1]))
+        }
+    }
+}
+data class Puzzle(val lines: List<Line>)
