f61c5468031577e1e2bd4c043189e2f54b2dee34
Adel <576853-astran@users.noreply.gitlab.com>
2021-12-06 09:30:53 +0100

    wip

--- src/main/kotlin/Day6.kt
+++ src/main/kotlin/Day6.kt
@@ -6,23 +6,34 @@ class Day6(path: String = "day6/input") {
         return School(inputData.flatMap { it.split(",") }.map(String::toInt))
     }
 }
+
 data class School(val fish: List<Int>) {
     fun advance(nbDay: Int): Long {
+        /*
         val days= MutableList(9){0L}
         fish.forEach {
             days[it]++
         }
-
+         */
+        val eachCount: Map<Int, Long> = fish.groupingBy { it }.eachCount().mapValues { it.value.toLong() }
+        val days = dayList(eachCount).toMutableList()
         repeat((0 until nbDay).count()) {
-            val newSix = days[0]
-            (1..8).forEach { i ->
-                days[i - 1] = days[i]
-            }
-            days[8] = newSix
-            days[6] += newSix
+            Collections.rotate(days, -1)
+            days[6] += days[8]
+        }
+
+        val map = (0 until nbDay ).fold(dayList(eachCount)) {acc,_->
+            val reproduction = acc[0]
+            val rotatedArray = acc.slice(1 until days.size) + reproduction
+            val newReproduction = dayList(mapOf(6 to reproduction, 8 to reproduction))
+            rotatedArray.zip(newReproduction){ a, b->a+b}
         }
         println(days)
         return days.sumOf { it }
+    }
+
+    private fun dayList(eachCount: Map<Int, Long>): List<Long> {
+        return (0..8).mapIndexed { i, _ -> eachCount.getOrDefault(i, 0) }.toList()
     }
 
 }
