7fe2d4492b2f4c2417c1e83001779a979c3cccd1
Adel <576853-astran@users.noreply.gitlab.com>
2021-12-06 07:48:27 +0100

    add implem

--- src/main/kotlin/Day6.kt
+++ src/main/kotlin/Day6.kt
@@ -1,16 +1,26 @@
 class Day6(path: String = "day6/input") {
     private val inputData: List<String> = path.fromResource().readLines()
     fun parse(): School {
-        return School(inputData.flatMap { it.split(",") }.map(String::toInt))
+        return School(inputData.flatMap { it.split(",") }.map(String::toInt).toSet())
     }
 }
 
-data class School(val fish: List<Int>) {
+data class School(val fish: Set<Int>) {
     fun advance(nbDay: Int): School {
-        return this
+        val newFish = (0 until nbDay).fold(fish) { acc, _ ->
+            acc.flatMap {
+                if (it == 0)
+                    listOf(6, 8)
+                else
+                    listOf(it - 1)
+            }.toSet()
+        }
+
+        return School(newFish)
     }
+
     fun count(): Int {
-        return fish.count()
+        return fish.size
     }
 }
 
