09fba27da602547fe783bcabec279e96436d0d80
Adel <576853-astran@users.noreply.gitlab.com>
2021-12-07 07:27:09 +0100

    done

--- src/main/kotlin/Day7.kt
+++ src/main/kotlin/Day7.kt
@@ -6,21 +6,18 @@ class Day7(path: String = "day7/input") {
         return Crabs(inputData.flatMap { it.split(",") }.map(String::toInt))
     }
 }
-
 fun Int.distance(i: Int) = (this - i).absoluteValue
 fun Int.intSum() = (this * (this + 1)) / 2
+fun Int.min(i: Int) = if (this < i) this else i
+fun Int.max(i: Int) = if (this > i) this else i
+fun Int.minMax(p: Pair<Int, Int>): Pair<Int, Int> = Pair(p.first.min(this), p.second.max(this))
 data class Crabs(val data: List<Int>) {
     fun align() = minValue(this::distanceTo)
     fun alignSum() = minValue(this::distanceToSum)
-
-
     fun distanceTo(target: Int) = data.sumOf { it.distance(target) }
-
     fun distanceToSum(target: Int) = data.sumOf { it.distance(target).intSum() }
-
     private fun minValue(fn: (Int) -> Int): Int {
-        val max = data.maxOf { it }
-        val min = data.minOf { it }
+        val (min, max) = data.fold(Pair(Int.MAX_VALUE, Int.MIN_VALUE)) { acc, i -> i.minMax(acc) }
         return (min..max).map(fn).minOf { it }
     }
 
