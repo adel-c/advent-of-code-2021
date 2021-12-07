bdde8dd74f7e5691c881e10bab77e2e6b9ff7285
Adel <576853-astran@users.noreply.gitlab.com>
2021-12-07 06:55:27 +0100

    wip

--- src/main/kotlin/Day7.kt
+++ src/main/kotlin/Day7.kt
@@ -1,3 +1,5 @@
+import kotlin.math.absoluteValue
+
 class Day7(path: String = "day7/input") {
     private val inputData: List<String> = path.fromResource().readLines()
     fun parse(): Crabs {
@@ -7,4 +9,5 @@ class Day7(path: String = "day7/input") {
 
 data class Crabs(val data: List<Int>) {
     fun align(): Int = 0
+    fun distanceTo(target: Int) = data.sumOf { (it - target).absoluteValue }
 }
--- src/test/kotlin/Day7Test.kt
+++ src/test/kotlin/Day7Test.kt
@@ -1,8 +1,30 @@
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 import kotlin.test.assertEquals
 
 
 class Day7Test {
+
+    companion object {
+        @JvmStatic
+        fun testData() = listOf(
+            Arguments.of(listOf(5), 0, 5),
+            Arguments.of(listOf(5), 1, 4),
+            Arguments.of(listOf(2), 5, 3),
+            Arguments.of(listOf(5, 6), 3, 5),
+            Arguments.of(listOf(1, 2, 3), 5, 9),
+
+            )
+    }
+
+    @ParameterizedTest(name = "{index} distance from {1} should be {2}  for {0}")
+    @MethodSource("testData")
+    fun should_compute_distance(data: List<Int>, to: Int, expected: Int) {
+        val s = Crabs(data)
+        assertEquals(expected, s.distanceTo(to))
+    }
 
     @Test
     fun test_data_should_be_37() {
