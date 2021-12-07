699ee2e4b06d81fdd0406dcc81a788aefbe33799
Adel <576853-astran@users.noreply.gitlab.com>
2021-12-07 07:39:32 +0100

    gen test files

--- src/main/kotlin/InitDayX.kt
+++ src/main/kotlin/InitDayX.kt
@@ -13,7 +13,37 @@ fun main(args: Array<String>) {
         }
     """.trimIndent())
 
+    blancInput(inputFolder)
+
+
+    File("./src/test/kotlin/${className}Test.kt").writeText("""
+        import org.junit.jupiter.api.Test
+        import kotlin.test.assertEquals
+
+
+        class ${className}Test {
+
+            @Test
+            fun test_compute_should_be_X() {
+                val day = $className("$inputFolder/inputTest")
+                assertEquals(0, day.compute())
+            }
+
+            @Test
+            fun compute_should_be_X() {
+                val day = $className("$inputFolder/input")
+                assertEquals(0, day.compute())
+            }
+        }
+    """.trimIndent())
+}
+
+private fun blancInput(inputFolder: String) {
     val data = File("./src/main/resources/$inputFolder")
     data.mkdirs()
-    File(data,"input").writeText("")
+    File(data, "input").writeText("")
+
+    val dataTest = File("./src/test/resources/$inputFolder")
+    dataTest.mkdirs()
+    File(dataTest, "inputTest").writeText("")
 }
