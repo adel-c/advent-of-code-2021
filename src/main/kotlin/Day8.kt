9e870fd8659156b250305ae93c74d7db894039a7
Adel CHEBBINE <adel.chebbine@external.grtgaz.com>
2021-12-08 10:02:53 +0100

    wip

--- src/main/kotlin/Day8.kt
+++ src/main/kotlin/Day8.kt
@@ -1,3 +1,5 @@
+import java.util.*
+
 class Day8(path: String = "day8/input") {
     private val inputData: List<String> = path.fromResource().readLines()
     private val data = inputData.map { it.split("|") }.map { DataLine(it[0].split(" "), it[1].split(" ")) }
@@ -25,29 +27,41 @@ enum class DIGIT(val representation: String) {
     EIGHT("abcdefg"),
     NINE("abcdfg");
 
+    fun size() = representation.length
 
     companion object {
         private val uniqueSizeDigit: Map<Int, List<DIGIT>> = values().groupBy { it.representation.length }
 
-        fun isUnique(value: String): Boolean {
-            return uniqueSizeDigit.getOrDefault(value.length, listOf()).size == 1
+        fun isUnique(value: SortedSet<Char>): Boolean {
+            return uniqueSizeDigit.getOrDefault(value.size, listOf()).size == 1
         }
 
-        fun possibilitiesBySize(value: String): List<DIGIT> {
-            return uniqueSizeDigit.getOrDefault(value.length, listOf())
+        fun possibilitiesBySize(value: SortedSet<Char>): List<DIGIT> {
+            return uniqueSizeDigit.getOrDefault(value.size, listOf())
         }
+
     }
 
 }
 
-data class DataLine(val data: List<String>, val output: List<String>) {
+data class DataLine(val dataUnSorted: List<String>, val outputUnSorted: List<String>) {
+    val data: List<SortedSet<Char>> = dataUnSorted.map { it.toSortedSet() }.toList()
+    val output: List<SortedSet<Char>> = outputUnSorted.map { it.toSortedSet() }.toList()
     fun numberOfOneFourSevenEight(): Int {
-        return output.count(DIGIT::isUnique)
+        return output.count{DIGIT.isUnique(it)}
+    }
+
+    fun possibilitiesBySize(data: List<SortedSet<Char>>): Map<Int, List<SortedSet<Char>>> {
+        return data.groupBy { it.size }
     }
 
     fun computeNumber(): Int {
-
-        val uniqueDigit: Map<String, List<DIGIT>> = data.associateWith { DIGIT.possibilitiesBySize(it) }
+        val possibilitiesBySize = possibilitiesBySize(data)
+        var one = possibilitiesBySize.getOrDefault(DIGIT.ONE.size(),"")
+        var seven = possibilitiesBySize.getOrDefault(DIGIT.SEVEN.size(),"")
+        var four = possibilitiesBySize.getOrDefault(DIGIT.FOUR.size(),"")
+        var eight = possibilitiesBySize.getOrDefault(DIGIT.EIGHT.size(),"")
+       // val uniqueDigit: Map<String, List<DIGIT>> = data.associateWith { DIGIT.possibilitiesBySize(it) }
         return -1
     }
 }
