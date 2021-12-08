9fd00e016f962633adb1f93014d319109cd6daef
Adel <576853-astran@users.noreply.gitlab.com>
2021-12-08 07:15:18 +0100

    add tests

--- src/main/kotlin/Day8.kt
+++ src/main/kotlin/Day8.kt
@@ -1,6 +1,31 @@
 class Day8(path: String = "day8/input") {
     private val inputData: List<String> = path.fromResource().readLines()
     fun compute(): Int {
-        return 0
+        val data = inputData.map { it.split("|") }.map { DataLine(it[0].split(" "), it[1].split(" ")) }
+        println(data)
+        return data.map(DataLine::numberOfOneFourSevenEight).sum()
+    }
+}
+
+enum class DIGIT(val size: Int) {
+    ONE(2),
+    FOUR(4),
+    SEVEN(3),
+    EIGHT(7);
+
+    companion object {
+        val uniqueSizeDigit = values().groupingBy { it.size }.eachCount()
+        fun hasUniqueSize(i: Int): Boolean {
+
+            return uniqueSizeDigit.getOrDefault(i, -1) == 1
+        }
+    }
+
+}
+
+data class DataLine(val data: List<String>, val output: List<String>) {
+    fun numberOfOneFourSevenEight(): Int {
+
+        return output.map { it.length }.filter(DIGIT::hasUniqueSize).sum()
     }
 }
--- src/test/kotlin/Day8Test.kt
+++ src/test/kotlin/Day8Test.kt
@@ -1,8 +1,44 @@
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 import kotlin.test.assertEquals
 
 
 class Day8Test {
+
+
+    companion object {
+        @JvmStatic
+        fun testData() = listOf(
+            Arguments.of(2,listOf("be", "cfbegad", "cbdgef", "fgaecd", "cgeb", "fdcge", "agebfd", "fecdb", "fabcd", "edb"), listOf("fdgacbe", "cefdb", "cefbgd", "gcbe")),
+            Arguments.of(3,listOf("edbfga", "begcd", "cbg", "gc", "gcadebf", "fbgde", "acbgfd", "abcde", "gfcbed", "gfec"), listOf("fcgedb", "cgb", "dgebacf", "gc")),
+            Arguments.of(3,listOf("fgaebd", "cg", "bdaec", "gdafb", "agbcfd", "gdcbef", "bgcad", "gfac", "gcb", "cdgabef"), listOf("cg", "cg", "fdcagb", "cbg")),
+            Arguments.of(1,listOf("fbegcd", "cbd", "adcefb", "dageb", "afcb", "bc", "aefdc", "ecdab", "fgdeca", "fcdbega"), listOf("efabcd", "cedba", "gadfec", "cb")),
+            Arguments.of(3,listOf("aecbfdg", "fbg", "gf", "bafeg", "dbefa", "fcge", "gcbea", "fcaegb", "dgceab", "fcbdga"), listOf("gecf", "egdcabf", "bgf", "bfgea")),
+            Arguments.of(4,listOf("fgeab", "ca", "afcebg", "bdacfeg", "cfaedg", "gcfdb", "baec", "bfadeg", "bafgc", "acf"), listOf("gebdcfa", "ecba", "ca", "fadegcb")),
+            Arguments.of(3,listOf("dbcfg", "fgd", "bdegcaf", "fgec", "aegbdf", "ecdfab", "fbedc", "dacgb", "gdcebf", "gf"), listOf("cefg", "dcbef", "fcge", "gbcadfe")),
+            Arguments.of(1,listOf("bdfegc", "cbegaf", "gecbf", "dfcage", "bdacg", "ed", "bedf", "ced", "adcbefg", "gebcd"), listOf("ed", "bcgafe", "cdgba", "cbgef")),
+            Arguments.of(4,listOf("egadfb", "cdbfeg", "cegd", "fecab", "cgb", "gbdefca", "cg", "fgcdab", "egfdb", "bfceg"), listOf("gbdfcae", "bgc", "cg", "cgb")),
+            Arguments.of(2,listOf("gcafb", "gcf", "dcaebfg", "ecagb", "gf", "abcdeg", "gaef", "cafbge", "fdbac", "fegbdc"), listOf("fgae", "cfgab", "fg", "bagce")),
+
+            )
+
+    }
+
+    @ParameterizedTest(name = "{index} expect {0} from output {2}")
+    @MethodSource("testData")
+    fun test_line_by_line(expected: Int,data: List<String>, output:List<String> ) {
+        val s = DataLine(data,output)
+        assertEquals(expected, s.numberOfOneFourSevenEight())
+    }
+
+    @Test
+    fun test_lie() {
+        val day = Day8("day8/inputTest")
+        assertEquals(26, day.compute())
+    }
+
 
     @Test
     fun test_compute_should_be_X() {
