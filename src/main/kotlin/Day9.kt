b08084dfc1c2ee7fca19f0ea3e4ac9a510e70382
Adel <576853-astran@users.noreply.gitlab.com>
2021-12-09 09:38:41 +0100

    add tests

--- src/main/kotlin/Day9.kt
+++ src/main/kotlin/Day9.kt
@@ -5,6 +5,11 @@ class Day9(path: String = "day9/input") {
             Heightmap(inputData.filter(String::isNotEmpty).map { it.toCharArray().map { c -> c.toString().toInt() } })
         return heightmap.lowest()
     }
+    fun compute2(): Int {
+        val heightmap =
+            Heightmap(inputData.filter(String::isNotEmpty).map { it.toCharArray().map { c -> c.toString().toInt() } })
+        return heightmap.largestBasins()
+    }
 }
 
 
@@ -13,9 +18,12 @@ data class Heightmap(val data: List<List<Int>>) {
 
 
     fun lowest(): Int {
-
         val lowPoints = lowPoints()
+        return lowPoints.sumOf { it.value + 1 }
+    }
 
+    fun largestBasins(): Int {
+        val lowPoints = lowPoints()
         return lowPoints.sumOf { it.value + 1 }
     }
 
@@ -31,6 +39,36 @@ data class Heightmap(val data: List<List<Int>>) {
     private fun right(i: Int, j: Int) = if (j < data[i].size - 1) data[i][j + 1] else -1
     private fun allLoc(i: Int, j: Int) =
         listOf(up(i, j), down(i, j), left(i, j), right(i, j)).filter { it != -1 }
+
+
+    private fun allUp(i: Int, j: Int) = sequence {
+        for (x in i downTo 0){
+            val value = data[x][j]
+            if(value == 9) return@sequence
+            yield(DataPoint(x,j,value))
+        }
+    }
+    private fun allDown(i: Int, j: Int) = sequence {
+        for (x in i until data.size){
+            val value = data[x][j]
+            if(value == 9) return@sequence
+            yield(DataPoint(x,j,value))
+        }
+    }
+    private fun allLeft(i: Int, j: Int) = sequence {
+        for (y in j downTo 0){
+            val value = data[i][y]
+            if(value == 9) return@sequence
+            yield(DataPoint(i,y,value))
+        }
+    }
+    private fun allRight(i: Int, j: Int) = sequence {
+        for (y in j until data.size){
+            val value = data[i][y]
+            if(value == 9) return@sequence
+            yield(DataPoint(i,y,value))
+        }
+    }
 
 
     private fun eachData() = sequence {
--- src/test/kotlin/Day9Test.kt
+++ src/test/kotlin/Day9Test.kt
@@ -15,4 +15,16 @@ class Day9Test {
         val day = Day9("day9/input")
         assertEquals(478, day.compute())
     }
+
+    @Test
+    fun test_compute2_should_be_X() {
+        val day = Day9("day9/inputTest")
+        assertEquals(1134, day.compute2())
+    }
+
+    @Test
+    fun compute2_should_be_X() {
+        val day = Day9("day9/input")
+        assertEquals(478, day.compute2())
+    }
 }
