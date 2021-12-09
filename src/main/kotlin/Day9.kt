8542990e130c86164ba9093d96578be0e1d9f195
Adel <576853-astran@users.noreply.gitlab.com>
2021-12-09 10:02:12 +0100

    wip

--- src/main/kotlin/Day9.kt
+++ src/main/kotlin/Day9.kt
@@ -1,3 +1,5 @@
+import java.util.*
+
 class Day9(path: String = "day9/input") {
     private val inputData: List<String> = path.fromResource().readLines()
     fun compute(): Int {
@@ -5,6 +7,7 @@ class Day9(path: String = "day9/input") {
             Heightmap(inputData.filter(String::isNotEmpty).map { it.toCharArray().map { c -> c.toString().toInt() } })
         return heightmap.lowest()
     }
+
     fun compute2(): Int {
         val heightmap =
             Heightmap(inputData.filter(String::isNotEmpty).map { it.toCharArray().map { c -> c.toString().toInt() } })
@@ -14,7 +17,7 @@ class Day9(path: String = "day9/input") {
 
 
 data class DataPoint(val i: Int, val j: Int, val value: Int)
-data class Basin(val points:List<DataPoint>)
+data class Basin(val points: Set<DataPoint>)
 data class Heightmap(val data: List<List<Int>>) {
 
 
@@ -30,9 +33,17 @@ data class Heightmap(val data: List<List<Int>>) {
     }
 
 
-    fun basinFromPoint(p:DataPoint):Basin{
+    fun basinFromPoint(p: DataPoint): Basin {
         val sequence: Sequence<DataPoint> = allDown(p.i, p.j) + allUp(p.i, p.j) + allLeft(p.i, p.j) + allRight(p.i, p.j)
-        return Basin(sequence.toList())
+
+        // allLoc(p.i,p.j).
+        return Basin(setOf())
+    }
+
+    fun all(p: DataPoint): List<Point> {
+
+
+        return listOf()// allLoc(p.i,p.j).filter { it }.flatMap { all(p) }
     }
 
     private fun lowPoints(): Sequence<DataPoint> = eachData().filter { point ->
@@ -49,32 +60,48 @@ data class Heightmap(val data: List<List<Int>>) {
         listOf(up(i, j), down(i, j), left(i, j), right(i, j)).filter { it != -1 }
 
 
+    private fun up(p: DataPoint) =
+        if (p.i > 0) Optional.of(DataPoint(p.i - 1, p.j, data[p.i - 1][p.j])) else Optional.empty<DataPoint>()
+
+    private fun down(p: DataPoint) =
+        if (p.i < data.size - 1) Optional.of(DataPoint(p.i + 1, p.j, data[p.i + 1][p.j])) else Optional.empty()
+
+    private fun left(p: DataPoint) =
+        if (p.j > 0) Optional.of(DataPoint(p.i, p.j - 1, data[p.i][p.j - 1])) else Optional.empty()
+
+    private fun right(p: DataPoint) =
+        if (p.j < data[p.i].size - 1) Optional.of(DataPoint(p.i, p.j + 1, data[p.i][p.j + 1])) else Optional.empty()
+
+
     private fun allUp(i: Int, j: Int) = sequence {
-        for (x in i-1 downTo 0){
+        for (x in i - 1 downTo 0) {
             val value = data[x][j]
-            if(value == 9) return@sequence
-            yield(DataPoint(x,j,value))
+            if (value == 9) return@sequence
+            yield(DataPoint(x, j, value))
         }
     }
+
     private fun allDown(i: Int, j: Int) = sequence {
-        for (x in i+1 until data.size){
+        for (x in i + 1 until data.size) {
             val value = data[x][j]
-            if(value == 9) return@sequence
-            yield(DataPoint(x,j,value))
+            if (value == 9) return@sequence
+            yield(DataPoint(x, j, value))
         }
     }
+
     private fun allLeft(i: Int, j: Int) = sequence {
-        for (y in j-1 downTo 0){
+        for (y in j - 1 downTo 0) {
             val value = data[i][y]
-            if(value == 9) return@sequence
-            yield(DataPoint(i,y,value))
+            if (value == 9) return@sequence
+            yield(DataPoint(i, y, value))
         }
     }
+
     private fun allRight(i: Int, j: Int) = sequence {
-        for (y in j+1 until data.size){
+        for (y in j + 1 until data.size) {
             val value = data[i][y]
-            if(value == 9) return@sequence
-            yield(DataPoint(i,y,value))
+            if (value == 9) return@sequence
+            yield(DataPoint(i, y, value))
         }
     }
 
