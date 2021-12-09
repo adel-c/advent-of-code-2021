4ec728d990da56da729ef9848c2efcec8171bb83
Adel <576853-astran@users.noreply.gitlab.com>
2021-12-09 08:46:39 +0100

    wip

--- src/main/kotlin/Day9.kt
+++ src/main/kotlin/Day9.kt
@@ -1,6 +1,12 @@
 class Day9(path: String = "day9/input") {
     private val inputData: List<String> = path.fromResource().readLines()
     fun compute(): Int {
-        return 0
+        val heightmap =
+            Heightmap(inputData.filter(String::isEmpty).map { it.toCharArray().map { c -> c.toString().toInt() } })
+        return heightmap.lowest()
     }
+}
+
+data class Heightmap(val data: List<List<Int>>) {
+    fun lowest(): Int = 0
 }
