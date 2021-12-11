5a29187279149b01027d21a1d950bc53c8bfc94a
Adel <576853-astran@users.noreply.gitlab.com>
2021-12-11 10:50:23 +0100

    wip

--- src/main/kotlin/Day11.kt
+++ src/main/kotlin/Day11.kt
@@ -8,11 +8,24 @@ class Day11(path: String = "day11/input") {
         return 0
     }
 }
-data class Pos(val i:Int,val j:Int)
-data class OctoGrid(val grid: List<MutableList<Int>>) {
 
+data class OctoGrid(val grid: List<MutableList<Int>>) {
+    private val matrix= Matrix(grid)
     fun countFlashes(): Int {
-        val flashed = mutableSetOf <Pos>()
+
+        (1..100).forEach { _ ->
+            val flashed = mutableSetOf <Point>()
+            var flashedSize = 0
+            matrix.map { it.value+1 }
+
+            do {
+                val flashedInIteration = matrix.eachData().filter { it.value > 9 }.map { it.point() }
+
+
+            } while (flashedSize != flashed.size)
+
+        }
+
         return 0
     }
 }
--- src/main/kotlin/MatrixUtils.kt
+++ src/main/kotlin/MatrixUtils.kt
@@ -2,30 +2,39 @@ import java.util.*
 
 
 data class DataPoint(val i: Int, val j: Int, val value: Int) {
-
+    fun point() = Point(i, j)
 }
 
-data class Matrix(val data: List<List<Int>>) {
-     fun up(p: DataPoint) = oPoint(p.i - 1, p.j)
+private infix fun Int.progressTo(b: Int) = if (this < b) this..b else this downTo b
+data class Point(val i: Int, val j: Int) {
+
+    fun isDiagonal(p: Point): Boolean = (i != p.i) && (j != p.j)//((x - p.x).absoluteValue == (y - p.y).absoluteValue)
+    fun progressionX(p: Point) = i progressTo p.i
+    fun progressionY(p: Point) = j progressTo p.j
+}
+
+data class Matrix(private val oData: List<List<Int>>) {
+    val data: List<MutableList<Int>> = oData.map { it.toMutableList() }
+    fun up(p: DataPoint) = oPoint(p.i - 1, p.j)
 
 
-     fun down(p: DataPoint) = oPoint(p.i + 1, p.j)
+    fun down(p: DataPoint) = oPoint(p.i + 1, p.j)
 
 
-     fun left(p: DataPoint) = oPoint(p.i, p.j - 1)
+    fun left(p: DataPoint) = oPoint(p.i, p.j - 1)
 
 
-     fun right(p: DataPoint) = oPoint(p.i, p.j + 1)
+    fun right(p: DataPoint) = oPoint(p.i, p.j + 1)
 
 
-     fun upLeft(p: DataPoint) = oPoint(p.i - 1, p.j - 1)
-     fun upRight(p: DataPoint) = oPoint(p.i - 1, p.j + 1)
-     fun downLeft(p: DataPoint) = oPoint(p.i + 1, p.j - 1)
-     fun downRight(p: DataPoint) = oPoint(p.i + 1, p.j + 1)
-     fun aroundNoDiag(p: DataPoint) =
+    fun upLeft(p: DataPoint) = oPoint(p.i - 1, p.j - 1)
+    fun upRight(p: DataPoint) = oPoint(p.i - 1, p.j + 1)
+    fun downLeft(p: DataPoint) = oPoint(p.i + 1, p.j - 1)
+    fun downRight(p: DataPoint) = oPoint(p.i + 1, p.j + 1)
+    fun aroundNoDiag(p: DataPoint) =
         listOf(up(p), down(p), left(p), right(p)).filter { it.isPresent }.map { it.get() }
 
-     fun around(p: DataPoint) =
+    fun around(p: DataPoint) =
         listOf(
             up(p),
             down(p),
@@ -37,7 +46,7 @@ data class Matrix(val data: List<List<Int>>) {
             downRight(p)
         ).filter { it.isPresent }.map { it.get() }
 
-    private fun oPoint(i: Int, j: Int) = if (i >= 0 && i < data.size  && j >= 0 && j < data[i].size ) Optional.of(
+    private fun oPoint(i: Int, j: Int) = if (i >= 0 && i < data.size && j >= 0 && j < data[i].size) Optional.of(
         DataPoint(
             i,
             j,
@@ -45,13 +54,22 @@ data class Matrix(val data: List<List<Int>>) {
         )
     ) else Optional.empty()
 
-
-     fun eachData() = sequence {
+    fun eachData() = sequence {
         for (i in data.indices) {
             for (j in data[i].indices) {
                 val current = data[i][j]
                 yield(DataPoint(i, j, current))
             }
         }
+    }
+
+    fun map(fn: (DataPoint) -> Int) {
+        eachData().forEach {
+            data[it.i][it.j] = fn(it)
+        }
+    }
+
+    fun set(p: Point, value: Int) {
+        data[p.i][p.j] = value
     }
 }
