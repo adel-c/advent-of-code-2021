d73b6f8d3a9ce95d369f6e2799c672655db5eeb8
Adel <576853-astran@users.noreply.gitlab.com>
2021-12-12 10:59:27 +0100

    fix parser

--- src/main/kotlin/Day12.kt
+++ src/main/kotlin/Day12.kt
@@ -3,26 +3,40 @@ class Day12(path: String = "day12/input") {
     fun compute(): Int {
         return Paths(parse()).path1()
     }
+
     fun compute2(): Int {
         return 0
     }
-    fun parse(): Map<String,Cave> {
-        return inputData.asSequence().filter { it.isNotBlank() }
+
+    fun parse(): Map<String, Cave> {
+        val map = inputData.asSequence().filter { it.isNotBlank() }
             .map { it.split("-") }
             .map { Cave(it[0]) to Cave(it[1]) }
-            .map { it.first.pathTo.add(it.second)
-            it.first}
-            .map { it.name to it }
-            .toMap()
 
+
+    return map.fold(mutableMapOf()){
+            acc, pair ->
+
+        if (!acc.containsKey(pair.first.name)) {
+            acc[pair.first.name] = pair.first
+        }
+        if (!acc.containsKey(pair.second.name)) {
+            acc[pair.second.name] = pair.second
+        }
+
+        acc[pair.first.name]!!.pathTo.add(acc[pair.second.name]!!)
+        acc
+    }
     }
 }
-data class Cave(val name:String){
-    val big= name.uppercase() == name
-    val pathTo= mutableSetOf<Cave>()
+
+data class Cave(val name: String) {
+    val big = name.uppercase() == name
+    val pathTo = mutableSetOf<Cave>()
 }
-class Paths(val map:Map<String,Cave>){
-    fun path1():Int{
+
+class Paths(val map: Map<String, Cave>) {
+    fun path1(): Int {
         return 0
     }
 }
