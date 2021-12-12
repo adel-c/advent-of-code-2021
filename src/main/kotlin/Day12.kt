5e29b9110e64d13b17d40d3d9ceec5d7020a3fbb
Adel <576853-astran@users.noreply.gitlab.com>
2021-12-12 11:36:33 +0100

    WIP

--- src/main/kotlin/Day12.kt
+++ src/main/kotlin/Day12.kt
@@ -1,3 +1,5 @@
+import java.util.*
+
 class Day12(path: String = "day12/input") {
     private val inputData: List<String> = path.fromResource().readLines()
     fun compute(): Int {
@@ -14,19 +16,18 @@ class Day12(path: String = "day12/input") {
             .map { Cave(it[0]) to Cave(it[1]) }
 
 
-    return map.fold(mutableMapOf()){
-            acc, pair ->
+        return map.fold(mutableMapOf()) { acc, pair ->
 
-        if (!acc.containsKey(pair.first.name)) {
-            acc[pair.first.name] = pair.first
-        }
-        if (!acc.containsKey(pair.second.name)) {
-            acc[pair.second.name] = pair.second
-        }
+            if (!acc.containsKey(pair.first.name)) {
+                acc[pair.first.name] = pair.first
+            }
+            if (!acc.containsKey(pair.second.name)) {
+                acc[pair.second.name] = pair.second
+            }
 
-        acc[pair.first.name]!!.pathTo.add(acc[pair.second.name]!!)
-        acc
-    }
+            acc[pair.first.name]!!.pathTo.add(acc[pair.second.name]!!)
+            acc
+        }
     }
 }
 
@@ -37,6 +38,45 @@ data class Cave(val name: String) {
 
 class Paths(val map: Map<String, Cave>) {
     fun path1(): Int {
-        return 0
+        val endCave = map["end"]!!
+        val paths = mutableListOf<List<Cave>>()
+        val stack = Stack<Cave>()
+        val deadEnd = mutableSetOf<Cave>()
+        stack.push(map["start"]!!)
+        while (canMove(stack)) {
+            val currentCave = stack.peek()
+
+            var notVisited = currentCave.pathTo
+                .filter { !stack.filter(this::multiple).toList().contains(it) }
+                .filter { !deadEnd.contains(it) }
+
+
+            if (notVisited.contains(endCave)) {
+                val element = stack.toList()
+                paths.add(element + endCave)
+            }
+            notVisited = notVisited - endCave
+            if (notVisited.isNotEmpty()) {
+                val toVisit = notVisited[0]
+                stack.push(toVisit)
+            } else {
+                deadEnd.add(currentCave)
+                stack.pop()
+
+            }
+
+        }
+        return paths.size
+    }
+
+    private fun path2(){
+
+    }
+    private fun multiple(toVisit: Cave): Boolean {
+        return !toVisit.big || toVisit.name == "start" || toVisit.name == "end"
+    }
+
+    private fun canMove(stack: Stack<Cave>): Boolean {
+        return stack.isNotEmpty()
     }
 }
