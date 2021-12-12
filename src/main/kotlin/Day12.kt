63dcdea43f741c8c7363f45419e0292311ef1df5
Adel <576853-astran@users.noreply.gitlab.com>
2021-12-12 10:31:52 +0100

    add day 12 stub

---
+++ src/main/kotlin/Day12.kt
@@ -0,0 +1,9 @@
+class Day12(path: String = "day12/input") {
+    private val inputData: List<String> = path.fromResource().readLines()
+    fun compute(): Int {
+        return 0
+    }
+    fun compute2(): Int {
+        return 0
+    }
+}
---
+++ src/test/kotlin/Day12Test.kt
@@ -0,0 +1,29 @@
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+
+class Day12Test {
+
+    @Test
+    fun test_compute_should_be_X() {
+        val day = Day12("day12/inputTest")
+        assertEquals(0, day.compute())
+    }
+
+    @Test
+    fun compute_should_be_X() {
+        val day = Day12("day12/input")
+        assertEquals(0, day.compute())
+    }
+    @Test
+    fun test2_compute_should_be_X() {
+        val day = Day12("day12/inputTest")
+        assertEquals(0, day.compute2())
+    }
+
+    @Test
+    fun compute2_should_be_X() {
+        val day = Day12("day12/input")
+        assertEquals(0, day.compute2())
+    }
+}
