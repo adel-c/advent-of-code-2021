b7e48b542bc182366d988b056fa92a781208b14c
Adel CHEBBINE <adel.chebbine@external.grtgaz.com>
2021-12-14 10:25:36 +0100

    fail

--- src/main/kotlin/Day14.kt
+++ src/main/kotlin/Day14.kt
@@ -1,43 +1,64 @@
+import java.util.*
+
 class Day14(path: String = "day14/input") {
     private val inputData: List<String> = path.fromResource().readLines()
-    fun compute(): Int {
+    fun compute(): Long {
         val (template, map) = parse()
         return Polym(template, map).count1()
     }
 
-    fun compute2(): Int {
+    fun compute2(): Long {
         val (template, map) = parse()
         return Polym(template, map).count2()
     }
 
-    fun parse(): Pair<String, Map<String, String>> {
+    fun parse(): Pair<String, Map<String, Char>> {
         val template = inputData[0]
-        val toMap = inputData.filter { it.contains("->") }.map { it.split(" -> ") }.associate { it[0] to it[1] }
+        val toMap = inputData.filter { it.contains("->") }.map { it.split(" -> ") }
+            .associate { it[0] to it[1].toCharArray()[0] }
         return Pair(template, toMap)
     }
 
-    data class Polym(val template: String, val map: Map<String, String>) {
-        fun count2(): Int {
+    data class Polym(val template: String, val map: Map<String, Char>) {
+        fun count2(): Long {
             return iteration(40)
         }
 
-        fun count1(): Int {
-            return iteration()
+        fun count1(): Long {
+            return iteration(20)
         }
 
-        private fun iteration(iteration: Int = 10): Int {
+        private fun iteration(iteration: Int = 10): Long {
             var ori = template
+            var ori2 = template
             for (i in 1..iteration) {
                 ori = it1(ori)
+                //ori2 = it2(ori2)
+                //println("$i \n$ori\n$ori2")
             }
-            val charCounts = ori.toCharArray().groupBy { it }.mapValues { it.value.count() }.values
-            val (min, max) = charCounts.fold(Pair(Int.MAX_VALUE, Int.MIN_VALUE)) { acc, i -> i.minMax(acc) }
-
+            val charCounts = ori.toCharArray().groupBy { it }.mapValues { it.value.count().toLong() }.values
+            val (min, max) = charCounts.fold(Pair(Long.MAX_VALUE, Long.MIN_VALUE)) { acc, i -> i.minMax(acc) }
 
             return max - min
         }
 
         private fun it1(ori: String): String {
+            val queue = ArrayDeque(ori.toCharArray().toList())
+            val result = Stack<Char>()
+            result.push(queue.pop())
+            while (queue.isNotEmpty()) {
+                val firstChar = queue.pop()
+                val secondChar = result.peek()
+                val key = "$secondChar$firstChar"
+                if (map.containsKey(key)) {
+                    result.push(map[key])
+                }
+                result.push(firstChar)
+            }
+            return result.joinToString("")
+        }
+
+        private fun it2(ori: String): String {
             var ori1 = ori
             var temp =
                 ori1.windowed(2).map { convert(it) }.mapIndexed { index, s -> if (index != 0) s.substring(1) else s }
@@ -49,9 +70,10 @@ class Day14(path: String = "day14/input") {
 
         private fun convert(value: String): String {
 
-            return if (map.containsKey(value)) value[0] + map.getOrDefault(value, "") + value[1]
+            return if (map.containsKey(value)) "${value[0]}${map.get(value)!!}${value[1]}"
             else value
 
         }
+
     }
 }
--- src/main/kotlin/Utils.kt
+++ src/main/kotlin/Utils.kt
@@ -10,8 +10,12 @@ fun getResourcePath(path: String): String {
     return if (resource != null) resource.path
     else throw Exception()
 }
+
 fun Int.distance(i: Int) = (this - i).absoluteValue
 fun Int.intSum() = (this * (this + 1)) / 2
 fun Int.min(i: Int) = if (this < i) this else i
 fun Int.max(i: Int) = if (this > i) this else i
 fun Int.minMax(p: Pair<Int, Int>): Pair<Int, Int> = Pair(p.first.min(this), p.second.max(this))
+fun Long.minMax(p: Pair<Long, Long>): Pair<Long, Long> = Pair(p.first.min(this), p.second.max(this))
+fun Long.min(i: Long) = if (this < i) this else i
+fun Long.max(i: Long) = if (this > i) this else i
