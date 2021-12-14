f82500ea51a25e4d561069c5a57cedd37c19bca3
Adel CHEBBINE <adel.chebbine@external.grtgaz.com>
2021-12-14 08:57:39 +0100

    add stub

---
+++ src/main/kotlin/Day14.kt
@@ -0,0 +1,10 @@
+class Day14(path: String = "day14/input") {
+    private val inputData: List<String> = path.fromResource().readLines()
+    fun compute(): Int {
+        return 0
+    }
+
+    fun compute2(): Int {
+        return 0
+    }
+}
---
+++ src/test/kotlin/Day14Test.kt
@@ -0,0 +1,30 @@
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+
+class Day14Test {
+
+    @Test
+    fun test_compute_should_be_X() {
+        val day = Day14("day14/inputTest")
+        assertEquals(0, day.compute())
+    }
+
+    @Test
+    fun compute_should_be_X() {
+        val day = Day14("day14/input")
+        assertEquals(0, day.compute())
+    }
+
+    @Test
+    fun test2_compute_should_be_X() {
+        val day = Day14("day14/inputTest")
+        assertEquals(0, day.compute2())
+    }
+
+    @Test
+    fun compute2_should_be_X() {
+        val day = Day14("day14/input")
+        assertEquals(0, day.compute2())
+    }
+}
