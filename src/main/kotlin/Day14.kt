11630c4ed0de41761fb17570ad0a6edbbe57e0eb
Adel CHEBBINE <adel.chebbine@external.grtgaz.com>
2021-12-14 11:19:07 +0100

    wip33

--- src/main/kotlin/Day14.kt
+++ src/main/kotlin/Day14.kt
@@ -1,5 +1,3 @@
-import kotlin.collections.ArrayList
-
 class Day14(path: String = "day14/input") {
     private val inputData: List<String> = path.fromResource().readLines()
     fun compute(): Long {
@@ -12,16 +10,17 @@ class Day14(path: String = "day14/input") {
         return Polym(template, map).count2()
     }
 
-    fun parse(): Pair<String, Map<String, Char>> {
+    fun parse(): Pair<String, Map<Pair<Char, Char>, Char>> {
         val template = inputData[0]
         val toMap = inputData.filter { it.contains("->") }.map { it.split(" -> ") }
             .associate { it[0] to it[1].toCharArray()[0] }
-        return Pair(template, toMap)
+        val mapKeys = toMap.mapKeys { Pair(it.key.toCharArray()[0], it.key.toCharArray()[1]) }
+        return Pair(template, mapKeys)
     }
 
-    data class Polym(val template: String, val map: Map<String, Char>) {
+    data class Polym(val template: String, val map: Map<Pair<Char, Char>, Char>) {
         fun count2(): Long {
-            return iteration(40)
+            return iteration(25)
         }
 
         fun count1(): Long {
@@ -33,8 +32,8 @@ class Day14(path: String = "day14/input") {
             var ori2 = template
             for (i in 1..iteration) {
                 ori = it1(ori)
-                ori2 = it2(ori2)
-                println("$i \n$ori\n$ori2")
+                //ori2 = it2(ori2)
+                //println("$i \n$ori\n$ori2")
             }
             val charCounts = ori.groupBy { it }.mapValues { it.value.count().toLong() }.values
             val (min, max) = charCounts.fold(Pair(Long.MAX_VALUE, Long.MIN_VALUE)) { acc, i -> i.minMax(acc) }
@@ -47,8 +46,8 @@ class Day14(path: String = "day14/input") {
             result.add(ori[0])
             for (i in 1..ori.lastIndex) {
                 val firstChar = ori[i]
-                val secondChar = result.last()
-                val key = "$secondChar$firstChar"
+                val secondChar = ori[i - 1]
+                val key = Pair(secondChar, firstChar)
                 if (map.containsKey(key)) {
                     result.add(map[key]!!)
                 }
@@ -69,7 +68,8 @@ class Day14(path: String = "day14/input") {
 
         private fun convert(value: String): String {
 
-            return if (map.containsKey(value)) "${value[0]}${map.get(value)!!}${value[1]}"
+            val key = Pair(value[0], value[1])
+            return if (map.containsKey(key)) "${value[0]}${map.get(key)!!}${value[1]}"
             else value
 
         }
