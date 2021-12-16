68a9d5ed723fe579263c08624cb87618812e0bcb
Adel <576853-astran@users.noreply.gitlab.com>
2021-12-16 07:46:55 +0100

    day15

--- src/main/kotlin/Day15.kt
+++ src/main/kotlin/Day15.kt
@@ -1,9 +1,26 @@
 class Day15(path: String = "day15/input") {
     private val inputData: List<String> = path.fromResource().readLines()
     fun compute(): Int {
-        return 0
+        return ShortestPath(parse()).shortPath1()
     }
+
     fun compute2(): Int {
-        return 0
+        return ShortestPath(parse()).shortPath2()
+    }
+
+    fun parse(): Matrix {
+        return Matrix(inputData.filter { it.isNotBlank() }.map { it.split("") }.map { it.map { v -> v.toInt() } })
+    }
+
+
+    data class ShortestPath(val matrix:Matrix){
+        fun shortPath1(): Int {
+           return 0
+        }
+
+        fun shortPath2(): Int {
+          return 0
+        }
+
     }
 }
