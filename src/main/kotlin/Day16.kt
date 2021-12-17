e15013f8973b8ea0962f646dca69f93b6e458e83
Adel <576853-astran@users.noreply.gitlab.com>
2021-12-17 08:10:50 +0100

    day16 litteral parser

--- src/main/kotlin/Day16.kt
+++ src/main/kotlin/Day16.kt
@@ -3,24 +3,36 @@ class Day16(path: String = "day16/input") {
 
 
     fun compute(): Int {
-        println(hexToBin())
-        return PacketParser(hexToBin()).count()
+        println(hexToBin(inputData[0]))
+        return PacketParser(hexToBin(inputData[0])).count()
     }
 
     fun compute2(): Int {
         return 0
     }
 
-    fun hexToBin(): String {
-        return inputData[0].toCharArray().map { hexCharToBinary(it.toString()) }.joinToString("")
+    companion object{
+        fun hexToBin(input:String): String {
+            return input.toCharArray().map { hexCharToBinary(it.toString()) }.joinToString("")
+        }
+
+        fun hexCharToBinary(hex: String): String {
+            val i = hex.toInt(16)
+            return Integer.toBinaryString(i).padStart(4, '0')
+        }
     }
 
-    fun hexCharToBinary(hex: String): String {
-        val i = hex.toInt(16)
-        return Integer.toBinaryString(i).padStart(4, '0')
-    }
 
-    data class Packet(val version: Int, val typeId: Int, val binaryMessage: String)
+    open class Packet(open val version: Int, open val typeId: Int)
+    data class LiteralPacket(override val version: Int, override val typeId: Int, val value: String) :
+        Packet(version, typeId)
+
+    data class OperatorPacket(
+        override val version: Int,
+        override val typeId: Int,
+        val subPacket: List<Packet>
+    ) : Packet(version, typeId)
+
     data class PacketParser(val binaryMessage: String) {
         fun String.binToInt() = this.toInt(2)
         fun parse(): Packet {
@@ -42,7 +54,11 @@ class Day16(path: String = "day16/input") {
         }
 
         private fun parseLiteralPacket(version: Int, typeId: Int, binaryMessage: String): Packet {
-            TODO("Not yet implemented")
+            val packets = binaryMessage.substring(6)
+            val chunked = packets.chunked(5)
+            val count = chunked.takeWhile { it[0] != '0' }.size + 1
+            val value = chunked.subList(0, count).joinToString("") { it.substring(1) }.binToInt().toString()
+           return LiteralPacket(version,typeId,value)
         }
 
         private fun parseOperatorPacket(version: Int, typeId: Int, binaryMessage: String): Packet {
--- src/test/kotlin/Day16Test.kt
+++ src/test/kotlin/Day16Test.kt
@@ -5,6 +5,13 @@ import kotlin.test.assertEquals
 class Day16Test {
 
     @Test
+    fun parseLitteral() {
+
+        val day = Day16.PacketParser(Day16.hexToBin("D2FE28"))
+        assertEquals(Day16.LiteralPacket(6,4,"2021"), day.parse())
+    }
+
+    @Test
     fun test_compute_should_be_X() {
         val day = Day16("day16/inputTest")
         assertEquals(16, day.compute())
