0c0ae59a7a2e5b1405b7098452ec64875f6e3dd3
Adel <576853-astran@users.noreply.gitlab.com>
2021-12-17 07:22:26 +0100

    day16

---
+++ src/main/kotlin/Day16.kt
@@ -0,0 +1,9 @@
+class Day16(path: String = "day16/input") {
+    private val inputData: List<String> = path.fromResource().readLines()
+    fun compute(): Int {
+        return 0
+    }
+    fun compute2(): Int {
+        return 0
+    }
+}
