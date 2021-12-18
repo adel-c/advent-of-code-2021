1e96a0432ba033b56248443782a5540a31d772d7
Adel <576853-astran@users.noreply.gitlab.com>
2021-12-18 08:29:30 +0100

    add parser

--- src/main/kotlin/Day18.kt
+++ src/main/kotlin/Day18.kt
@@ -3,7 +3,58 @@ class Day18(path: String = "day18/input") {
     fun compute(): Long {
         return 0
     }
+
     fun compute2(): Long {
         return 0
+    }
+
+    fun String.isNumeric()=this.toIntOrNull() != null
+
+    fun numberParser(number: String): SnailValue {
+        if(number.isNumeric()){
+            return SnailNumber(number.toInt())
+        }
+        var level = 0
+        var list = mutableListOf<Char>()
+        var leftValue = ""
+        var rightValue = ""
+        for (c: Char in number.toCharArray()) {
+            when (c) {
+                '[' -> {
+                    level++
+                    if (level != 1) {
+                        list.add(c)
+                    }
+                }
+                ']' -> {
+                    if (level == 1) {
+                        rightValue = list.joinToString("")
+                    }else{
+                        list.add(c)
+                    }
+
+                    level--
+                }
+                ',' -> {
+                    if (level == 1) {
+                        leftValue = list.joinToString("")
+                        list.clear()
+                    }else{
+                        list.add(c)
+                    }
+                }
+                else -> list.add(c)
+
+            }
+        }
+       return SnailPair(numberParser(leftValue),numberParser(rightValue))
+    }
+
+    open class SnailValue()
+    data class SnailNumber(val v: Int) : SnailValue()
+    data class SnailPair(val right: SnailValue, val left: SnailValue) : SnailValue() {
+        companion object {
+            fun of(a: Int, b: Int) = SnailPair(SnailNumber(a), SnailNumber(b))
+        }
     }
 }
--- src/test/kotlin/Day18Test.kt
+++ src/test/kotlin/Day18Test.kt
@@ -3,6 +3,30 @@ import kotlin.test.assertEquals
 
 
 class Day18Test {
+    @Test
+    fun parseSimplePair() {
+        val day = Day18()
+        assertEquals(Day18.SnailPair.of(1,2), day.numberParser("[1,2]"))
+    }
+    @Test
+    fun parseSimplePair2() {
+        val day = Day18()
+        assertEquals(Day18.SnailPair(Day18.SnailPair.of(1,2), Day18.SnailNumber(3)), day.numberParser("[[1,2],3]"))
+    }
+    @Test
+    fun parseSimplePair3() {
+        val day = Day18()
+        assertEquals(Day18.SnailPair(Day18.SnailPair.of(1,9), Day18.SnailPair.of(8,5)), day.numberParser("[[1,9],[8,5]]"))
+    }
+
+    @Test
+    fun parseSimplePair4() {
+        val day = Day18()
+        assertEquals(Day18.SnailPair(Day18.SnailPair(
+            Day18.SnailPair(Day18.SnailPair.of(1,2), Day18.SnailPair.of(3,4)),
+            Day18.SnailPair(Day18.SnailPair.of(5,6), Day18.SnailPair.of(7,8)),
+        ), Day18.SnailNumber(9)), day.numberParser("[[[[1,2],[3,4]],[[5,6],[7,8]]],9]"))
+    }
 
     @Test
     fun test_compute_should_be_X() {
