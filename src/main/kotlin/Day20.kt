51062d927850b5249cda72af83093840e4a203a2
Adel <576853-astran@users.noreply.gitlab.com>
2021-12-20 20:53:41 +0100

    d20 wip2

--- src/main/kotlin/Day20.kt
+++ src/main/kotlin/Day20.kt
@@ -9,17 +9,30 @@ class Day20(path: String = "day20/input") {
     fun compute(): Long {
         println(dataLine)
         var matrix = parseMatrix()
-
         (0..2).forEach {
-            var newMatrix = Matrix(matrix.data.map { it.toList() })
+            var temp = Matrix(matrix.data.map { it.toList() })
+            temp.map { dataPoint ->
+                val binValue =
+                    dataPoint.point().aroundMatrix().joinToString("") { matrix.getOrDefault(it, 0).value.toString() }
+                val index = binValue.toInt(2)
+                val newValue = dataLine[index]
+                newValue
+            }
+            matrix = temp
         }
-        return 0
+        matrix.print()
+        return matrix.eachData().filter { it.value ==1 }.count().toLong()
     }
 
     fun compute2(): Long {
         return 0
     }
-
+    fun Matrix.print(){
+        val print = this.data.joinToString("\n") { line ->
+            line.map { if (it == 1) '#' else '.' }.joinToString("")
+        }
+        println(print)
+    }
     fun Point.aroundMatrix(): List<Point> =
         listOf(
             this.copy(i = i - 1, j = j - 1),
