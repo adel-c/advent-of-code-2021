e16b70f024442be5707267e87ce3e0c10da101f7
Adel <576853-astran@users.noreply.gitlab.com>
2021-12-21 07:53:46 +0100

    d21.1 ok

--- src/main/kotlin/Day21.kt
+++ src/main/kotlin/Day21.kt
@@ -1,25 +1,69 @@
 class Day21(path: String = "day21/input") {
     private val inputData: List<String> = path.fromResource().readLines()
     fun compute(): Long {
-        val dice = Dice(100)
-        val take = dice.nextValue().take(5)
-        println(take.toList())
-        println(dice.nextValue().take(3).toList())
-        return 0
+        val playerOneStart = inputData[0].split(":")[1].trim().toInt()
+        val playerTwoStart = inputData[1].split(":")[1].trim().toInt()
+        println("$playerOneStart $playerTwoStart")
+        return Game(playerOneStart,playerTwoStart).play()
     }
 
     fun compute2(): Long {
         return 0
     }
 
+    data class Player(val initialPosition: Int, val caseCount: Int = 10) {
+        var currentPosition = initialPosition
+        var score: Long = 0
+
+        fun play(move: Int) {
+            val trueMove = move % caseCount
+            currentPosition += trueMove
+            if(currentPosition>10)currentPosition-=10
+            score += currentPosition
+        }
+
+        fun hasWon() = score >= 1000
+    }
+
+    data class Game(val playerOneStart: Int, val playerTwoStart: Int) {
+        private val dice = Dice(100)
+        private val player1 = Player(playerOneStart)
+        private val player2 = Player(playerTwoStart)
+        fun play():Long {
+            do {
+
+                player1.play(dice.nextThree())
+                if(player1.hasWon())break
+                player2.play(dice.nextThree())
+                if(player2.hasWon())break
+            } while (true)
+            val winnerScore=if(player1.hasWon())player2.score else player1.score
+
+            println(dice.totalThrows)
+            return winnerScore * dice.totalThrows
+        }
+
+        fun round() {
+
+        }
+    }
+
     data class Dice(val sideCount: Int = 100) {
+
         private var currentValue = 0
+        var totalThrows: Long = 0
         fun nextValue(): Sequence<Int> = sequence {
-            while(true){
-                yield(currentValue++ % sideCount)
+            while (true) {
+                totalThrows++
+                currentValue+=1
+                if(currentValue>sideCount)currentValue-=sideCount
+                yield(currentValue)
 
             }
+        }
 
+        fun nextThree():Int {
+            return nextValue().take(3).sum()
         }
     }
 }
