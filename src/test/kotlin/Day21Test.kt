e6e19b84bf8ac97fbce29b051b4c7726b850ca03
Adel <576853-astran@users.noreply.gitlab.com>
2021-12-21 07:02:01 +0100

    d21

---
+++ src/test/kotlin/Day21Test.kt
@@ -0,0 +1,29 @@
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+
+class Day21Test {
+
+    @Test
+    fun test_compute_should_be_X() {
+        val day = Day21("day21/inputTest")
+        assertEquals(739785, day.compute())
+    }
+
+    @Test
+    fun compute_should_be_X() {
+        val day = Day21("day21/input")
+        assertEquals(0, day.compute())
+    }
+    @Test
+    fun test2_compute_should_be_X() {
+        val day = Day21("day21/inputTest")
+        assertEquals(0, day.compute2())
+    }
+
+    @Test
+    fun compute2_should_be_X() {
+        val day = Day21("day21/input")
+        assertEquals(0, day.compute2())
+    }
+}
