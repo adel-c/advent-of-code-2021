88805d5e2ba4dd1e298d12db477097245a22c500
Adel CHEBBINE <adel.chebbine@external.grtgaz.com>
2021-12-23 08:26:28 +0100

    fail

--- src/main/kotlin/Day21.kt
+++ src/main/kotlin/Day21.kt
@@ -10,10 +10,24 @@ class Day21(path: String = "day21/input") {
     fun compute2(): Long {
         val playerOneStart = inputData[0].split(":")[1].trim().toInt()
         val playerTwoStart = inputData[1].split(":")[1].trim().toInt()
-        println("$playerOneStart $playerTwoStart")
-        return 0
+        val initialGame = QGame(Player(playerOneStart, 0, 21), Player(playerTwoStart, 0, 21))
+        val m = mutableListOf<QGame>().apply { add(initialGame) }
+        do {
+
+            val play = m.flatMap { it.play() }
+            m.addAll(play)
+        } while (play.isNotEmpty())
+
+        val fold = m.fold(Pair(0L, 0L)) { acc, qgame ->
+            val p1 = if (qgame.player1.hasWon()) 1 else 0
+            val p2 = if (qgame.player2.hasWon()) 1 else 0
+            Pair(acc.first + p1, acc.second + p2)
+        }
+        println("$fold")
+        return fold.max()
     }
 
+    fun Pair<Long, Long>.max() = if (first > second) first else second
     data class Player(val position: Int, val score: Int, val winningScore: Int = 1000, val caseCount: Int = 10) {
 
         fun play(move: Int): Player {
@@ -21,12 +35,43 @@ class Day21(path: String = "day21/input") {
             var newPos = position + trueMove
             if (newPos > 10) newPos -= 10
             val newScore = score + newPos
-            return Player(newPos, newScore)
+            return this.copy(position = newPos, score = newScore)
         }
 
         fun hasWon() = score >= winningScore
     }
 
+    data class QGame(val player1: Player, val player2: Player) {
+        private val dice = Dice(100)
+
+        fun play(): List<QGame> {
+            if (player1.hasWon() || player2.hasWon()) {
+                return emptyList()
+            }
+            var p1 = player1
+            var p2 = player2
+            val l = mutableListOf<QGame>()
+            for (i in 1..3) {
+                p1 = p1.play(i)
+                l.add(QGame(p1, p2))
+
+                if (p1.hasWon()) {
+                    break;
+                }
+                for (j in 1..3) {
+                    p2 = p2.play(j)
+                    l.add(QGame(p1, p2))
+                    if (p2.hasWon()) {
+                        break;
+                    }
+                }
+
+            }
+            return l
+        }
+
+
+    }
 
     data class Game(val playerOneStart: Int, val playerTwoStart: Int) {
         private val dice = Dice(100)
